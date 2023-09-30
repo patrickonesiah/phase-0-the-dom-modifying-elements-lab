68c07d358850a5b7cd556eefc75ef420a8c051cb
patrickonesiah <patrickonesiah@gmail.com>
2023-09-30 22:31:46 +1000

    Lab completed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const bodyElement = document.querySelector('body');
+bodyElement.querySelector('main').remove();
+const newHeader = document.createElement("H1");
+document.body.appendChild(newHeader);
+newHeader.id = 'victory'
+newHeader.innerHTML = 'asds is the champion'
